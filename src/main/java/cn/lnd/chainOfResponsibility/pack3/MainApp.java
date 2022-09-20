049a538eb0990535cfca20c16486b402785386dc
LvNengDong <993696248@qq.com>
2022-09-21 00:37:27 +0800

    手写EventBus

--- src/main/java/cn/lnd/chainOfResponsibility/pack3/MainApp.java
+++ src/main/java/cn/lnd/chainOfResponsibility/pack3/MainApp.java
@@ -6,10 +6,9 @@ import java.util.List;
 
 /**
  * @Author lnd
- * @Description
- *      <h1>
- *          原则：封装变化
- *      </h1>
+ * @Description <h1>
+ * 原则：封装变化
+ * </h1>
  * @Date 2022/8/21 19:40
  */
 public class MainApp {
@@ -24,15 +23,19 @@ public class MainApp {
         HTMLFilter htmlFilter = new HTMLFilter();
         SensitiveFilter sensitiveFilter = new SensitiveFilter();
         fc.add(htmlFilter).add(sensitiveFilter);
-        FilterChain fc2 = new FilterChain();
-        fc2.add(sensitiveFilter).add(htmlFilter);
-
-        System.out.println("");
+        //FilterChain fc2 = new FilterChain();
+        //fc2.add(sensitiveFilter).add(htmlFilter);
+        fc.doFilter(request, response, fc); //第一次调用chain#doFilter
+        System.out.println(request.getStr());
+        System.out.println(response.getStr());
+        /*
+            在 filterChain 中处理加入位置的记录，同时在 filter 中加入第三个参数
+        */
     }
 }
 
 /* 要传给后台服务器的信息 */
-class Message{
+class Message {
     String msg;
 
     public String getMsg() {
@@ -51,7 +54,8 @@ class Message{
     }
 
 }
-class Request{
+
+class Request {
     String str;
 
     @Override
@@ -69,8 +73,10 @@ class Request{
         this.str = str;
     }
 }
-class Response{
+
+class Response {
     String str;
+
     @Override
     public String toString() {
         return "Request{" +
@@ -87,35 +93,46 @@ class Response{
     }
 }
 
-interface Filter{
-    boolean doFilter(Request request, Response response);
+interface Filter {
+    boolean doFilter(Request request, Response response, FilterChain chain);
 }
 
-class HTMLFilter implements Filter{
+class HTMLFilter implements Filter {
 
     @Override
-    public boolean doFilter(Request request, Response response) {
-        String str = request.getStr() + "request filter1";
+    public boolean doFilter(Request request, Response response, FilterChain chain) {
+        String str = request.getStr() + "request filter1;";
         request.setStr(str);
+        // 如果不调用 chain#doFilter 方法，整个链路就不会继续往下执行了
+        chain.doFilter(request, response, chain);
+        response.setStr(response.getStr() + "--response filter1");
         return false;
     }
 }
 
-class SensitiveFilter implements Filter{
+class SensitiveFilter implements Filter {
+
     @Override
-    public boolean doFilter(Request request, Response response) {
-        return false;
+    public boolean doFilter(Request request, Response response, FilterChain chain) {
+        String str = request.getStr() + "request filter2;";
+        request.setStr(str);
+        chain.doFilter(request, response, chain);
+        response.setStr(response.getStr() + "--response filter2");
+        return true;
     }
 }
 
-class FilterChain implements Filter{
+class FilterChain implements Filter {
     List<Filter> filters = new ArrayList<>();
 
-    public FilterChain add(Filter filter){
+    int index = 0; //记录当前FilterChain执行到了哪一个Filter
+
+    public FilterChain add(Filter filter) {
         filters.add(filter);
         return this;
     }
-    public void delete(Filter filter){
+
+    public void delete(Filter filter) {
         filters.remove(filter);
     }
 
@@ -124,11 +141,17 @@ class FilterChain implements Filter{
      */
 
     @Override
-    public boolean doFilter(Request request, Response response) {
-        for (Filter filter : filters) {
-            boolean b = filter.doFilter(request, response);
-            if (!b) return false;
-        }
-        return true;
+    public boolean doFilter(Request request, Response response, FilterChain chain) {
+        //如果整个链条都执行完毕了，就结束
+        if (index == filters.size()) return false;
+        // 如果链条没执行完，就拿到应该执行的filter去执行
+        Filter filter = filters.get(index);
+        index++;
+        boolean b = filter.doFilter(request, response, chain);
+        return b;
     }
 }
+
+/*
+    类似于递归调用
+*/
--- src/main/java/cn/lnd/observer/demo04/ObserverRegistry.java
+++ src/main/java/cn/lnd/observer/demo04/ObserverRegistry.java
@@ -1,10 +1,65 @@
 package cn.lnd.observer.demo04;
 
+import com.google.common.base.Preconditions;
+
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 /**
  * @Author lnd
  * @Description ObserverRegistry 类就是前面讲到的 Observer 注册表，
  * @Date 2022/9/20 20:33
  */
 public class ObserverRegistry {
+    private ConcurrentHashMap<Class<?>, CopyOnWriteArraySet<ObserverAction>> registry = new ConcurrentHashMap<>();
 
+    /**
+     * 注册观察者
+     * @param observer 观察者
+     */
+    public void register(Object observer){
+        Map<Class<?>, Collection<ObserverAction>> observerActions = findAllObserverActions(observer);
+        for (Map.Entry<Class<?>, Collection<ObserverAction>> entry : observerActions.entrySet()) {
+            Class<?> eventType = entry.getKey();
+            Collection<ObserverAction> eventActions = entry.getValue();
+            CopyOnWriteArraySet<ObserverAction> registeredEventActions = registry.get(eventType);
+            if (registeredEventActions == null){
+                registry.put(eventType, new CopyOnWriteArraySet<>());
+                registeredEventActions = registry.get(eventType);
+            }
+            registeredEventActions.addAll(eventActions);
+        }
+    }
+
+    private Map<Class<?>, Collection<ObserverAction>> findAllObserverActions(Object observer) {
+        //Map中的key是Class类型，值是Set类型，也就是说：一个Class可以对应多个值，这也对应了一个event可以被多个观察者的方法订阅
+        Map<Class<?>, Collection<ObserverAction>> observerActions = new HashMap<>();
+        Class<?> clazz = observer.getClass();
+
+        for (Method method : getAnnotatedMethods(clazz)) { //得到含有@Subscribe注解的方法
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            Class<?> eventType = parameterTypes[0];
+            if (!observerActions.containsKey(eventType)){
+                observerActions.put(eventType, new ArrayList<>());
+            }
+            observerActions.get(eventType).add(new ObserverAction(observer, method));
+        }
+        return observerActions;
+    }
+
+    private List<Method> getAnnotatedMethods(Class<?> clazz) {
+        List<Method> annotatedMethods = new ArrayList<>();
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Subscribe.class)){
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                Preconditions.checkArgument(parameterTypes.length == 1,
+                        "Method %s has @Subscribe annotation but has %s parameters.Subscriber methods must have exactly 1 parameter.",
+                        method, parameterTypes.length);
+                annotatedMethods.add(method);
+            }
+        }
+        return annotatedMethods;
+    }
 }
